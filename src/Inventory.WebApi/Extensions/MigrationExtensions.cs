69cddb5c489d471eedae71485af90ce93760c3ce
José Miguel Alvarez <j.m.alvarez.camacho@gmail.com>
2026-02-28 12:21:12 -0400

    prueva de formateo

--- src/Inventory.WebApi/Extensions/MigrationExtensions.cs
+++ src/Inventory.WebApi/Extensions/MigrationExtensions.cs
@@ -9,7 +9,7 @@ public static class MigrationExtensions
         using IServiceScope scope = app.ApplicationServices.CreateScope();
 
         using var db =
-            scope.ServiceProvider.GetRequiredService<IDatabase>();
+                            scope.ServiceProvider.GetRequiredService<IDatabase>();
 
         db.Migrate();
     }
