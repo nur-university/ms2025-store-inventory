5e2cccb5bd70dc29827b51097b01e1dcbcc9c6ed
José Miguel Alvarez <j.m.alvarez.camacho@gmail.com>
2026-03-07 09:28:18 -0400

    subidos cambios

--- src/Inventory.WebApi/Extensions/MigrationExtensions.cs
+++ src/Inventory.WebApi/Extensions/MigrationExtensions.cs
@@ -6,7 +6,7 @@ public static class MigrationExtensions
 {
     public static void ApplyMigrations(this IApplicationBuilder app)
     {
-                        using IServiceScope scope = app.ApplicationServices.CreateScope();
+        using IServiceScope scope = app.ApplicationServices.CreateScope();
 
         using var db =
                         scope.ServiceProvider.GetRequiredService<IDatabase>();
